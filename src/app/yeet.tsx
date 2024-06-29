38e3859e1923dc025f0b4488a212f0da161507ae
Josh <joshconstantinelim@gmail.com>
2024-06-29 14:05:44 +1000

    upload

--- src/app/yeet.tsx
+++ src/app/yeet.tsx
@@ -1,9 +1,24 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import { UploadButton } from "./_components/uploadthing";
 
 export default function Yeet() {
   const a = useSession();
 
-  return <div>{JSON.stringify(a)}</div>;
+  return (
+    <div>
+      {JSON.stringify(a)}
+      <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          console.log("Files: ", res);
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+    </div>
+  );
 }
