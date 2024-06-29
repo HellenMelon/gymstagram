b0db705c20f54f1cdbc84423bca75eb1ad2055d8
Josh <joshconstantinelim@gmail.com>
2024-06-29 14:57:25 +1000

    hellen says make it prettier

--- src/app/auth/login/loginForm.tsx
+++ src/app/auth/login/loginForm.tsx
@@ -40,12 +40,12 @@ export default function LoginForm() {
       </div>
 
       <div>
-        <label
+        {/* <label
           htmlFor="password"
           className="block text-sm font-medium text-gray-700"
         >
           Password
-        </label>
+        </label> */}
         <div className="mt-1">
           <input
             id="password"
@@ -53,8 +53,9 @@ export default function LoginForm() {
             type="password"
             autoComplete="current-password"
             required
-            className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+            className="block w-full appearance-none border-b border-gray-300 py-2 placeholder-gray-400 shadow-sm transition-all focus:border-blue-500 focus:outline-none active:outline-none sm:text-sm"
             value={password}
+            placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,16 +3,16 @@ import LoginForm from "./loginForm";
 
 export default function Login() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <div className="flex flex-col items-center">
-        <Image
-          src="/logo.png"
-          alt="logo"
-          className="w-full max-w-64"
-          width={300}
-          height={300}
-        />
-        <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-lg">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-cyan-200 to-blue-500">
+      <div className="rounded-lg bg-white p-8 shadow-lg">
+        <div className="flex flex-col items-center">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            className="w-full max-w-64"
+            width={300}
+            height={300}
+          />
           <h2 className="mb-6 text-center text-2xl font-bold text-gray-900">
             Login to your account
           </h2>
